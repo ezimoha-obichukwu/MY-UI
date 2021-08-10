84a7adc67a791f46d7fa79dff435990a83c2ce49
ezimoha-obichukwu <77929524+ezimoha-obichukwu@users.noreply.github.com>
2021-08-10 16:37:33 +0100

    MY WEBSITE
    this is my website

---
+++ index3.js
@@ -0,0 +1,17 @@
+winndow.onload() => {
+    swal({
+        title: "Greeting",
+        text: 'Hey Sololearn Gangs',
+        icon: "Success",
+    }).then(_ => {
+        swal("what is this about?", 'This is a tutorial', "info");
+    })
+    $(function () {
+        $('a[href^="#]').on('click', function (e) {
+            var $target = $(this.getAttribute('href/'); of
+            if ($target.lenght) {
+                e.preventDefault9
+            }   )
+    })
+})
+}
